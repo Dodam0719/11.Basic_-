81a0d361127e2bbd4daef7e97fcb4eb3acf694c0
Dodam0719 <blueoh719@gmail.com>
2023-09-30 17:33:44 +0900

    연산자 - 논리 연산자 중 not(!)연산자

--- 3.기본_문법/index(3-6).js
+++ 3.기본_문법/index(3-6).js
@@ -1,16 +1,14 @@
-// || 연산자
-var o1 = true || true; // t || t는 true 반환
-var o2 = false || true; // f || t는 true 반환
-var o3 = true || false; // t || f는 true 반환
-var o4 = false || 3 == 4; // f || f는 false 반환
-var o5 = "Cat" || "Dog"; // t || t는 Cat 반환
-var o6 = false || "Cat"; // f || t는 Cat 반환
-var o7 = "Cat" || false; // t || f는 Cat 반환
+// && => and => 그리고!!
+// || => or => 또는!!
 
-console.log(o1); // true
-console.log(o2); // true
-console.log(o3); // true
-console.log(o4); // false
-console.log(o5); // Cat
-console.log(o6); // Cat
-console.log(o7); // Cat
+// ! 연산자
+var not1 = !true; // !t는 false 반환
+var not2 = !false; // !f는 true 반환
+var not3 = !"Cat"; // !t는 false 반환
+
+console.log(not1); // false
+console.log(not2); // true
+console.log(not3); // false
+
+not3 = !!"Cat"; // !! 두번 사용
+console.log(not3); // true
