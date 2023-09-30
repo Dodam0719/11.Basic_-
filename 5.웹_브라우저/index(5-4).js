ce67f0f37437bf24ae47f10efa68bd46490387ee
Dodam0719 <blueoh719@gmail.com>
2023-09-30 22:12:51 +0900

    DOM 선택하고 조작하기 - `h2` 조작하기(2)

--- 5.웹_브라우저/index(5-4).js
+++ 5.웹_브라우저/index(5-4).js
@@ -2,4 +2,8 @@ const h2Element = document.querySelector("h2");
 
 console.log(h2Element);
 
-h2Element.innerText = "안녕하세요";
+function replaceH2Text() {
+  h2Element.innerText = "안녕하세요";
+}
+
+replaceH2Text();
