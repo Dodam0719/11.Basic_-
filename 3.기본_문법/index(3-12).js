f90cf85d86f486ebbac29783b07c7e40033f30f6
Dodam0719 <blueoh719@gmail.com>
2023-09-30 21:13:16 +0900

    블럭 스코프 - let인 경우

--- 3.기본_문법/index(3-12).js
+++ 3.기본_문법/index(3-12).js
@@ -4,20 +4,20 @@
  * - 내부에서 외부로는 접근 O
  * - 외부에서 내부로는 접근 X
  *
- * 처음부터 => var
- * ES2015+ => let, const
+ * 처음부터 => var => 함수 단위 유효 범위
+ * ES2015+ => let, const => 블럭 단위
  */
 
 var globalVal = "함수-단위(전역 변수)";
 
-for (var index = 0; index < 3; index++) {}
+for (let index = 0; index < 3; index++) {}
 
 function outerFunc() {
   console.log(globalVal); // 함수-단위(전역 변수)
 
   function innerFunc() {
     var innerVal = "블럭-단위(함수 내부 지역 변수)";
-    console.log(index); // 3
+    console.log(index); // index is not defined
   }
   innerFunc();
 }
