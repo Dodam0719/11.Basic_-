432af66066757ab01026004f88fd0a4665954291
Dodam0719 <blueoh719@gmail.com>
2023-09-30 16:16:34 +0900

    값 - 데이터 및 구조 유형

--- 3.기본_문법/index(3-3).js
+++ 3.기본_문법/index(3-3).js
@@ -1,5 +1,19 @@
-// 문자열 값 (원시 값)
-var str = "word";
-str.toUpperCase();
+typeof instance === "undefined";
+typeof instance === "Boolean";
+typeof instance === "number";
+typeof instance === "string";
+typeof instance === "bigint";
+typeof instance === "symbol";
 
-console.log(str);
+const un = undefined;
+const nu = null;
+const bool = false;
+const num = 0;
+const str = "문자열";
+// 원시 값이 아닌 데이터 타입은 즉...! 레퍼런스, 객체 타입!
+
+console.log(typeof un); // undefined
+console.log(typeof nu); // object
+console.log(typeof bool); // boolean
+console.log(typeof num); // number
+console.log(typeof str); // string
