76025a76f3c945b98c0e113fabde1101f9eb7de9
Dodam0719 <blueoh719@gmail.com>
2023-09-30 21:18:21 +0900

    블럭 스코프 - var, let 비교

--- 3.기본_문법/index(3-12).js
+++ 3.기본_문법/index(3-12).js
@@ -10,16 +10,10 @@
 
 var globalVal = "함수-단위(전역 변수)";
 
-for (let index = 0; index < 3; index++) {}
-
-function outerFunc() {
-  console.log(globalVal); // 함수-단위(전역 변수)
-
-  function innerFunc() {
-    var innerVal = "블럭-단위(함수 내부 지역 변수)";
-    console.log(index); // index is not defined
-  }
-  innerFunc();
+if (true) {
+  var b = "b";
+  let a = "a";
 }
 
-outerFunc();
+console.log(b); // b
+console.log(a); // a is not defined
