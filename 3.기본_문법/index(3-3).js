617f7795109cc3171858378ad40143b5fa6d6e03
Dodam0719 <blueoh719@gmail.com>
2023-09-30 16:28:25 +0900

    값 - 값의 비교

--- 3.기본_문법/index(3-3).js
+++ 3.기본_문법/index(3-3).js
@@ -1,33 +1,24 @@
-typeof instance === "undefined";
-typeof instance === "Boolean";
-typeof instance === "number";
-typeof instance === "string";
-typeof instance === "bigint";
-typeof instance === "symbol";
-
 const un = undefined;
 const nu = null;
 const bool = false;
-const num = 0;
+const num1 = 0;
 const str = "문자열";
 // 원시 값이 아닌 데이터 타입은 즉...! 레퍼런스, 객체 타입!
 
-console.log(typeof un); // undefined
-console.log(typeof nu); // object
-console.log(typeof bool); // boolean
-console.log(typeof num); // number
-console.log(typeof str); // string
+/*
+ * 동등 연산자를 사용할 때
+ * 엄격한 비교 => ===
+ * 느슨한 비교 => ==
+ */
 
-const obj = {
-  name: "jang",
-};
-
-const arr = ["jang"];
-
-function func() {
-  return "jang";
+if (num1 == 0) {
+  console.log("num은 0입니다"); // num은 0입니다.
 }
 
-console.log(typeof obj); // object
-console.log(typeof arr); // object
-console.log(typeof func); // function
+// 좌항에는 문자열의 '0' == 우항에는 숫자의 0
+const num2 = "0";
+if (num2 === 0) {
+  console.log("num은 0입니다");
+} else {
+  console.log("num은 0이 아닙니다."); // num은 0이 아닙니다.
+}
