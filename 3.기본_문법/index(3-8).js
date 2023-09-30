f786a4f76374232c28aad252231938719685efc0
Dodam0719 <blueoh719@gmail.com>
2023-09-30 18:37:58 +0900

    문 - if문과 for문

--- 3.기본_문법/index(3-8).js
+++ 3.기본_문법/index(3-8).js
@@ -1,7 +1,9 @@
-// 초기화; 평가(참 => 실행, 거짓 => 다음으로); 매번 실행되는 평가식;
 const array = [1, 2, 3];
 
-for (let i = 0; i < array.length; i++) {
-  console.log(i); // 0, 1, 2
-  console.log(array[i]); // 1, 2, 3
+if (array.length === 3) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === 1) {
+      console.log(array[i]); // 1
+    }
+  }
 }
