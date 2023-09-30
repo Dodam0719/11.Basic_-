74fea99a2e6b30346f3192e0d506768712e4a14e
Dodam0719 <blueoh719@gmail.com>
2023-09-30 22:15:25 +0900

    DOM 선택하고 조작하기 - `h2` 조작하기(3) -> `h2`를 클릭하면 글자가 바뀌는 이벤트 적용

--- 5.웹_브라우저/index(5-4).js
+++ 5.웹_브라우저/index(5-4).js
@@ -1,9 +1,7 @@
 const h2Element = document.querySelector("h2");
 
-console.log(h2Element);
-
 function replaceH2Text() {
   h2Element.innerText = "안녕하세요";
 }
 
-replaceH2Text();
+h2Element.addEventListener("click", replaceH2Text);
