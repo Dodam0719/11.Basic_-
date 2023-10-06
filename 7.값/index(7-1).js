b029956613ff1e03281113453df0112df2722d18
Dodam0719 <blueoh719@gmail.com>
2023-10-06 12:46:58 +0900

    원시 값(은 불변하다)

--- 7.값/index(7-1).js
+++ 7.값/index(7-1).js
@@ -4,3 +4,17 @@
  * - 원시(Primitive)
  * - 객체(Reference)
  */
+
+let test = "string";
+console.log(test.toUpperCase()); // STRING
+console.log(test); // string
+
+test = test.toUpperCase();
+console.log(test); // STRING
+
+let bool = false;
+console.log(!bool); // true
+console.log(bool); // fase
+
+bool = !bool;
+console.log(bool); // true
