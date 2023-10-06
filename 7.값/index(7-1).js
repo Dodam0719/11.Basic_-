faca9753d356de522d2dd6b26926c9e661204331
Dodam0719 <blueoh719@gmail.com>
2023-10-06 13:00:07 +0900

    참조타입 - 객체, 배열

--- 7.값/index(7-1).js
+++ 7.값/index(7-1).js
@@ -5,17 +5,12 @@
  * - 객체, 참조(Reference)
  */
 
-const object = {};
-const array = [];
-function func() {}
+const object = {
+  name: "jang",
+  age: 11,
+  arr: [],
+  func: function () {},
+  child: {},
+};
 
-console.log(object instanceof Object); // true
-console.log(array instanceof Array); // true
-console.log(func instanceof Function); // true
-
-console.log(Object.prototype.toString.call(object)); // [object Object]
-console.log(Object.prototype.toString.call(array)); // [object Array]
-console.log(Object.prototype.toString.call(func)); // [object Function]
-
-console.log(typeof array); // object
-console.log(typeof func); // function
+const array = [[], 11, "jang", function () {}];
