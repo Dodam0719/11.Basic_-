7a91b720cabbb146490f5347035922803298a595
Dodam0719 <blueoh719@gmail.com>
2023-10-13 21:24:27 +0900

    Truthy (참 같은 값)

---
+++ 8.불리언/index(8-4).js
@@ -0,0 +1,46 @@
+/**
+ * Truthy (참 같은 값)
+ */
+
+if (true) {
+  console.log("참 입니다"); // 참 입니다
+}
+if ({}) {
+  console.log("참 입니다"); // 참 입니다
+}
+if ([]) {
+  console.log("참 입니다"); // 참 입니다
+}
+if (42) {
+  console.log("참 입니다"); // 참 입니다
+}
+if ("0") {
+  console.log("참 입니다"); // 참 입니다
+}
+if ("false") {
+  console.log("참 입니다"); // 참 입니다
+}
+if (new Date()) {
+  console.log("참 입니다"); // 참 입니다
+}
+if (-42) {
+  console.log("참 입니다"); // 참 입니다
+}
+if (12n) {
+  console.log("참 입니다"); // 참 입니다
+}
+if (3.14) {
+  console.log("참 입니다"); // 참 입니다
+}
+if (-3.14) {
+  console.log("참 입니다"); // 참 입니다
+}
+if (Infinity) {
+  console.log("참 입니다"); // 참 입니다
+}
+if (-Infinity) {
+  console.log("참 입니다"); // 참 입니다
+}
+
+console.log({} && "Hello"); // Hello
+console.log([] && "Hello"); // Hello
