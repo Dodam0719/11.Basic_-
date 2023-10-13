8be4b135efe62502455c6f6762b880aaadff076c
Dodam0719 <blueoh719@gmail.com>
2023-10-14 00:28:48 +0900

    산술 연산자

---
+++ 9.숫자/index(9-8).js
@@ -0,0 +1,23 @@
+/**
+ * # 산술 연산자
+ * + - / * %
+ */
+
+console.log(1 + 1); // 2
+console.log(2 - 1); // 1
+console.log(10 / 5); // 2
+console.log(10 % 2); // 0
+console.log(2 * 2); // 4
+
+// 증감 연산자
+let x = 5;
+console.log(++x); // 6
+console.log(--x); // 5
+console.log(x++); // 5
+console.log(x); // 6
+console.log(x--); // 6
+console.log(x); // 5
+
+// 연산자 우선순위
+console.log(3 + 10 * 2); // 23
+console.log((3 + 10) * 2); // 26
