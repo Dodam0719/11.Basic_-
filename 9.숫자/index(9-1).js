26270e067171c26d4bfa9f89069f25ad5ac2a2f5
Dodam0719 <blueoh719@gmail.com>
2023-10-13 21:54:32 +0900

    숫자 리터럴

---
+++ 9.숫자/index(9-1).js
@@ -0,0 +1,12 @@
+console.log(Number.MAX_SAFE_INTEGER); // 9007199254740991
+console.log(35); // 35
+console.log(3.141); // 3.141
+console.log(0xff); // 255
+console.log(5e2); // 500
+// number에 대해서 Number.isNaN()으로 검사하는 습관 들이면 큰 문제는 없음
+
+// 숫자 리터럴
+const num = 123.111;
+console.log(num); // 123.111
+console.log(num.toFixed()); // 123
+// 숫자는 많은 메서드를 함유하고 있어서, 그 중 아무거나 써도 됨
