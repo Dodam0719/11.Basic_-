a3c18305b8d3d98c28d18a6db62c9b60efac61e9
Dodam0719 <blueoh719@gmail.com>
2023-10-14 01:41:59 +0900

    템플릿 리터럴

---
+++ 10.문자열/index(10-6).js
@@ -0,0 +1,29 @@
+/**
+ * Template Literals
+ * 1. 멀티라인 (개행)이 자유로움
+ * 2. Basic String Formatting => 보간법을 활용 => 표현식을 문자열 포함 가능
+ * 3. HTML Escaping: 안전하게 사용하도록 도움(XSS, SQL Injection)
+ *
+ */
+
+const multiLine = "first Line \n last line";
+console.log(multiLine);
+// first Line
+// last line
+
+// ES2015+
+function genHello(name) {
+  return `안녕하세요
+  ${name}님
+  반갑습니다.`;
+}
+console.log(genHello("jang"));
+// 안녕하세요
+// jang님
+// 반갑습니다.
+
+function genDivison(innerText) {
+  return `<div>${innerText}</div>`;
+}
+const divTag = genDivison("Jang");
+body.innerHTML = divTag;
