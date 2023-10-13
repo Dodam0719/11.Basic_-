e723bdb060c76b1e034579c26600f323bbb84203
Dodam0719 <blueoh719@gmail.com>
2023-10-13 20:46:31 +0900

    불리언으로 변환

---
+++ 8.불리언/index(8-1).js
@@ -0,0 +1,17 @@
+/**
+ * Boolean
+ * ON: True
+ * OFF: False
+ */
+
+console.log(Boolean('STRING')) // true
+console.log(Boolean(!! 'STRING')) // true
+console.log(!false) // true
+console.log(!!0) // false
+console.log(!!1) // true
+
+if () {
+  //
+} else {
+  //
+}
