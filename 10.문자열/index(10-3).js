e7ffbcff00bf11b179b3f2efc369e58a0b85cd84
Dodam0719 <blueoh719@gmail.com>
2023-10-15 23:21:42 +0900

    문자열로 변환(설명 추가)

--- 10.문자열/index(10-3).js
+++ 10.문자열/index(10-3).js
@@ -1,3 +1,5 @@
+//문자열로 변환
+
 console.log(String(123)); // 123
 console.log(String(undefined)); // undefined
 console.log(String(null)); // null
@@ -5,6 +7,8 @@ console.log(String({})); // [object Object]
 console.log(String({ name: "jang" })); // [object Object]
 console.log(String([1, 2, 3])); // 1,2,3
 
-// JSON.stringify는 원시값을 변환할 때 사용
+//JSON.stringify을 사용하면, 문자열로 바로 찍는듯한 현상을 보여줌
+//단, JSON.stringify는 원시값을 변환할때만 씀
 console.log(JSON.stringify({ name: "jang" })); // {"name":"jang"}
 console.log(JSON.stringify([1, 2, 3])); // [1,2,3]
+//그러나 문자열로 변환하는 건 string으로 충분하다.
