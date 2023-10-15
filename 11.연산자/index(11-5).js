6c1e8c7db7eee8a67082d33fc192ffa21630b4a8
Dodam0719 <blueoh719@gmail.com>
2023-10-15 23:14:44 +0900

    instanceof(설명 추가)

--- 11.연산자/index(11-5).js
+++ 11.연산자/index(11-5).js
@@ -8,9 +8,18 @@ const func = function () {};
 
 console.log(obj instanceof Object); // true
 console.log(arr instanceof Array); // true
-console.log(arr instanceof Object); // true
 console.log(func instanceof Function); // true
-console.log(func instanceof Object); // true
 
 console.log(undefined instanceof Object); // false
 console.log(null instanceof Object); // false. null=>원시값
+//null은 원시값이라는게 증명됨
+//instanceof로 래퍼런스를 비교할 수 있고,
+//프리미타입 밸류로 객체같은 래퍼런스 밸류를 비교해낼 수 있다.
+
+//그리고 자바스크립트에서 Object 프로토타입 체인안에 많은 것들이 있기 때문에,
+//이러한 래퍼런스 타입에서 최상위인 Object로 비교를 하면 true가 나옴
+//수많은 객체들이 프로토타입 체인을 타고 최상위 객체로 Object를 가지고 있기 때문
+console.log(obj instanceof Object);
+console.log(arr instanceof Object);
+console.log(func instanceof Object);
+console.log(new Date() instanceof Object);
