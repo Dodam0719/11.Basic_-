55d0749043f18b1f8ba98c90d9f2bacc068f567b
Dodam0719 <blueoh719@gmail.com>
2023-10-15 11:45:13 +0900

    함수 선언

---
+++ 13.함수/index(13-1).js
@@ -0,0 +1,16 @@
+// 함수 선언문
+function sum(param1, param2) {
+  return param1 + param2;
+}
+console.log(sum(1, 2)); // 3
+
+const obj = {
+  // 메서드
+  prop: function () {},
+};
+
+// 생성자 함수
+function MyObj() {}
+
+// 화살표 함수 (ES2015+, ES6)
+const arrowFunc = () => {};
