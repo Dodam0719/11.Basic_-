9771d8967524674e5903b79f60189482645c86b2
Dodam0719 <blueoh719@gmail.com>
2023-10-15 11:53:21 +0900

    화살표 함수

---
+++ 13.함수/index(13-3).js
@@ -0,0 +1,13 @@
+// 화살표 함수(는 함수 선언문과 동작 방식이 다르다는 걸 염두)
+const arrowFunc = () => {
+  // 1. argument
+  // 2. this
+  return "";
+};
+
+const arrowFunc = () => "문자열";
+
+// 함수 선언문
+function name() {
+  return "문자열";
+}
