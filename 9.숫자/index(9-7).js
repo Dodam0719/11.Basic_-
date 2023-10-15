53e945a7c884127d9c3d6a829b8bce4f7c9d31f9
Dodam0719 <blueoh719@gmail.com>
2023-10-15 23:37:03 +0900

    정수로 변환(설명 추가)

--- 9.숫자/index(9-7).js
+++ 9.숫자/index(9-7).js
@@ -1,5 +1,19 @@
-console.log(parseInt("10")); // 10
-console.log(parseInt(undefined)); // NaN
-console.log(Number("10")); // 10
+//정수로 변환
 
-Number.MAX_SAFE_INTEGER;
+console.log(parseInt("10"));
+console.log(Number("10"));
+
+//Number.isInteger는 주어진 값이 정수인지 판별
+console.log(Number.isInteger("10"));
+
+//Number.isSafeInteger는 주어진 값이 안전한 정수인지 판별
+console.log(Number.isSafeInteger("10"));
+
+//radix가 undefined면 value를 내보내고.. 아니면 다른건 내보내는 함수
+//radix는 선택적인 매개변수라서 대부분 무시하는데, 몇진수인지도 꼭 넘겨주어야 함
+function parseInt2(value, radix) {
+  if (radix === undefined) {
+    return Number(value);
+  }
+  return;
+}
