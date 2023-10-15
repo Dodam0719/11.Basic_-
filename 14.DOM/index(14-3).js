524ead1990368ec825e90cac87a21b5d3f07c906
Dodam0719 <blueoh719@gmail.com>
2023-10-15 14:17:59 +0900

    DOM 속성

---
+++ 14.DOM/index(14-3).js
@@ -0,0 +1,15 @@
+/**
+ * <li id="some-id" class="some-class"></li>
+ */
+
+const li = document.querySelector("li");
+const li = {
+  ariaAutocomplete: null,
+  ariaBusy: null,
+  ariaChecked: null,
+  ariaColCount: null,
+  innerHTML: '<a id="skip-main" href="#content">Skip to main...</a>',
+  appendChild: function () {
+    // ... Some Code
+  },
+};
